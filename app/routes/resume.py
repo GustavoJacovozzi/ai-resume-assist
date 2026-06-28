0f80eb15f47ee7e850142ac6f1d27ca47a076926
Gustavo <gustavojacovozzi@gmail.com>
2026-06-28 16:55:14 -0300

    feat: add PDF upload and text extraction

--- app/routes/resume.py
+++ app/routes/resume.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.resume import Resume
 from app.schemas.resume import ResumeCreate, ResumeUpdate
-
+import fitz
 
 router = APIRouter(
     prefix="/resume",
@@ -11,7 +11,7 @@ router = APIRouter(
 )
 
 @router.post("/")
-def upload_curriculo(
+def criar_curriculo(
     dados: ResumeCreate,
     db: Session = Depends(get_db)
 ):
@@ -86,3 +86,46 @@ def deletar_curriculo(
     return{
         "Mensagem": "Curriculo Removido com Sucesso"
     }
+
+@router.put("/{resume_id}")
+def atualizar_curriculo(
+    resume_id: int,
+    dados: ResumeUpdate,
+    db:Session = Depends(get_db)
+):
+    curriculo = (
+        db.query(Resume)
+        .filter(Resume.id==resume_id)
+        .first()
+    )
+    if curriculo is None:
+        raise HTTPException(
+            status_code=404,
+            detail= "Curriculo Não encontrado"
+
+        )
+    curriculo.candidate_name = dados.candidate_name
+    curriculo.email = dados.email
+    curriculo.skills = dados.skills
+    curriculo.resume_text = dados.resume_text
+    db.commit()
+    db.refresh()
+    return{
+        "Mensagem": "Curriculo Atualizado com sucesso",
+        "Curriculo": curriculo
+    }
+           
+@router.post("/upload")
+async def upload_curriculo(
+    arquivo: UploadFile = File(...)
+
+):
+    pdf_byte = await arquivo.read()
+    pdf = fitz.open(stream=pdf_byte, filetype="pdf")
+    texto = ""
+    for pagina in pdf:
+        texto += pagina.get_text()
+    return{
+        "nome": arquivo.filename,
+        "texto": texto
+    }   
